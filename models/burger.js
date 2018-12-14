7ae58c6cba4b54eeeb71ea4818708e541db3dc20
Jobi Mathai <jobi.mathai@gmail.com>
2018-12-14 00:27:33 -0800

    whole bunc of stuff

--- models/burger.js
+++ models/burger.js
@@ -1,27 +1,23 @@
-//import orm
+var orm = require("../config/orm.js");
 
-var orm = require("../config/orm");
-
-// The variables cols and vals are arrays.
 var burger = {
-  all: function(cb) {
-    orm.all("burgers", function(res) {
-      cb(res);
-    });
-  },
+	SelectAll: function(cb){
+		orm.selectAll("burgers", function(res){
+			cb(res);
+		})
+	},
 
-  create: function(cols, vals, cb) {
-    orm.create("burgers", cols, vals, function(res) {
-      cb(res);
-    });
-  },
+	updateOne: function(col, newVal, condition, cb) {
+		orm.updateOne("burgers", col, newVal, condition, function(res){
+			cb(res);
+		});
+	},
 
-  update: function(objColVals, condition, cb) {
-    orm.update("burgers", objColVals, condition, function(res) {
-      cb(res);
-    });
-  }
-};
+	insertOne: function(col, val, cb) {
+		orm.insertOne("burgers", col, val, function(res){
+			cb(res);
+		});
+	}
+}
 
-// Export the database functions for the controller
 module.exports = burger;
---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require("express");
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+// var db = require("./models");
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require('./controllers/burgers_controller.js');
+app.use('/', routes);
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
